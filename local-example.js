464ba132d8c194956873ec3c91f72665867f8aea
APL <ajanth160899@gmail.com>
2019-03-01 15:55:52 +0100

    Setup basic structure for sequelizejs

---
+++ local-example.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "VTimeline",
+  database: {
+    name: "DATABASE_NAME",
+    host: "DATABASE_HOST",
+    port: "DATABASE_PORT",
+    username: "DATABASE_USERNAME",
+    password: "DATABASE_PASSWORD",
+    dialect: "mysql",
+    logging: false
+  }
+};
